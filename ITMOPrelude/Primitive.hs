5e74beb9f4d69da5cca24b3f80e2af051692bfaa
Alina Dimitrova <alino4ka366@gmail.com>
2012-10-21 14:13:29 +0300

    Primitive with Algebra

--- ITMOPrelude/Primitive.hs
+++ ITMOPrelude/Primitive.hs
@@ -2,7 +2,7 @@
 module ITMOPrelude.Primitive where
 
 import Prelude (Show,Read,error)
-
+import ITMOPrelude.Algebra
 ---------------------------------------------
 -- Синтаксис лямбда-выражений
 
@@ -76,6 +76,15 @@ infixr 2 ||
 True || _ = True
 False || x = x
 
+instance Monoid Bool where
+	mempty = True
+	mappend = (&&)
+	
+data OrBool = Or Bool
+
+instance Monoid OrBool where
+	mempty = Or False
+	mappend (Or a) (Or b) = Or (a || b)
 -------------------------------------------
 -- Натуральные числа
 
@@ -144,6 +153,15 @@ natMod n = snd . natDivMod n -- Остаток
 gcd :: Nat -> Nat -> Nat
 gcd n m= gcd m (natMod n m)
 
+instance Monoid Nat where
+	mempty = Zero
+	mappend = (+.)
+	
+data MulNat = MulN Nat
+
+instance Monoid MulNat where
+	mempty = MulN Zero
+	mappend (MulN a) (MulN b) = MulN (a *. b)
 -------------------------------------------
 -- Целые числа
 
@@ -244,6 +262,20 @@ infixl 7 .*.
 (Pos n) .*. (Pos m) = Pos (uMul n m)
 (Neg n) .*. (Neg m) = Pos (uMul n m)
 
+instance Monoid Int where
+	mempty = IntZero
+	mappend = (.+.)
+	
+instance Group Int where
+	gempty = IntZero
+	gappend = (.+.)
+	greverse = intNeg
+	
+data MulInt = MulI Int
+
+instance Monoid MulInt where
+	mempty = MulI intOne
+	mappend (MulI a) (MulI b) = MulI (a .*. b)
 -------------------------------------------
 -- Рациональные числа
 
@@ -282,6 +314,25 @@ infixl 7 %*, %/
 (%/) :: Rat -> Rat -> Rat
 n %/ m = n %* (ratInv m)
 
+instance Monoid Rat where
+	mempty = Rat IntZero natOne
+	mappend = (%+)
+	
+instance Group Rat where
+	gempty = Rat IntZero natOne
+	gappend = (%+)
+	greverse = ratNeg
+	
+data MulRat = MulR Rat
+
+instance Monoid MulRat where
+	mempty = MulR (Rat intOne natOne)
+	mappend (MulR a) (MulR b) = MulR (a %* b)
+	
+instance Group MulRat where
+	gempty = MulR (Rat IntZero natOne)
+	gappend (MulR a) (MulR b) = MulR (a %* b)
+	greverse (MulR a) = MulR (ratInv a)
 -------------------------------------------
 -- Операции над функциями.
 -- Определены здесь, но использовать можно и выше
